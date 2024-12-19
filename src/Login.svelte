97f7cdf9b0e31afd217376abf9ac485efe475a50
sebastianpitre <sebastianandrespitrediaz@gmail.com>
2024-12-19 18:06:28 -0500

    go

--- src/Login.svelte
+++ src/Login.svelte
@@ -3,64 +3,61 @@
   import Carga from './components/Carga.svelte';
 
   let showOverlay = true;
-
-  onMount(() => {
-      setTimeout(() => {
-          showOverlay = false;
-      }, 16500); // 8 segundos
-  });
-
+  let isLoading = false;
   let videoElement;
 
-    function playWithAudio() {
-        videoElement.play(); // Asegura que el video se reproduzca
-    }
+  // Función para iniciar el video con audio
+  function playWithAudio() {
+    videoElement.play();
+  }
 
-    //Carga de la página
+  onMount(() => {
+    // Esperar a que el video esté listo
+    videoElement.onloadeddata = () => {
+      playWithAudio(); // Inicia el video con audio
+      setTimeout(() => {
+        showOverlay = false; // Oculta la superposición después de 16.5 segundos
+      }, 16500);
+    };
+  });
 
-    let isLoading = false;
+  // Función para manejar el clic en el enlace
+  function handleClick(event, url) {
+    event.preventDefault(); // Evita la navegación inmediata
+    isLoading = true; // Muestra la animación de carga
 
-    // Función para manejar el clic en el enlace
-    function handleClick(event, url) {
-        event.preventDefault(); // Evita la navegación inmediata
-        isLoading = true; // Muestra la animación de carga
-
-        // Espera 2 segundos antes de redirigir
-        setTimeout(() => {
-            window.location.href = url; // Redirige a la nueva dirección
-        }, 2000);
-    }
+    setTimeout(() => {
+      window.location.href = url; // Redirige a la nueva dirección
+    }, 6000);
+  }
 </script>
-<main>
 
+<main>
   {#if isLoading}
-    <Carga/>
+    <Carga />
   {/if}
-  
 
   <div class="fullscreen-container">
     {#if showOverlay}
-        <!-- Div superpuesto -->
-        <div class="overlay">
-          <h6 class="loading-text text-white" style="z-index: 9999;">Create by Sebastian pitre</h6>
-          <!-- svelte-ignore a11y-media-has-caption -->
-          <video autoplay loop id="myVideo" bind:this={videoElement}>
+      <!-- Div superpuesto -->
+      <div class="overlay">
+        <h6 class="loading-text text-white" style="z-index: 9999;">Create by Sebastian Pitre</h6>
+        <!-- svelte-ignore a11y-media-has-caption -->
+        <video autoplay muted id="myVideo" bind:this={videoElement}>
             <source src="/video/go.mp4" type="video/mp4">
-          </video>
-          <button class="unmute-button" on:click={playWithAudio}>Activar sonido</button>
-        </div>
+        </video>
+      </div>
     {/if}
-  
+
     <a href="/home" on:click={(event) => handleClick(event, '/home')}>
       <div class="background"></div>
     </a>
   </div>
-
 </main>
 
 <style>
   /* Contenedor principal que ocupa toda la pantalla */
-.fullscreen-container {
+  .fullscreen-container {
     position: relative;
     width: 100vw;
     height: 100vh;
@@ -68,10 +65,10 @@
     display: flex;
     justify-content: center;
     align-items: center;
-}
+  }
 
-/* Imagen de fondo */
-.background {
+  /* Imagen de fondo */
+  .background {
     position: absolute;
     top: 0;
     left: 0;
@@ -80,17 +77,17 @@
     background: url('/img/login.png') no-repeat center center;
     background-size: cover;
     z-index: 1;
-}
+  }
 
-video{
-  position: absolute;
-  top: 0;
-  left: 0;
-  height: 100%;
-}
+  video {
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+  }
 
-/* Div superpuesto */
-.overlay {
+  /* Div superpuesto */
+  .overlay {
     position: absolute;
     top: 0;
     left: 0;
@@ -103,23 +100,23 @@ video{
     align-items: center;
     color: white;
     font-size: 2rem;
-}
+  }
 
-/* Texto de carga */
-.loading-text {
-  position: absolute;
-  left: 10px;
-  bottom: 10px;
+  /* Texto de carga */
+  .loading-text {
+    position: absolute;
+    left: 10px;
+    bottom: 10px;
     animation: blink 1.5s infinite; /* Animación de parpadeo */
-}
+  }
 
-@keyframes blink {
-    0%, 100% {
-        opacity: 1;
+  @keyframes blink {
+    0%,
+    100% {
+      opacity: 1;
     }
     50% {
-        opacity: 0.5;
+      opacity: 0.5;
     }
-}
-
+  }
 </style>
